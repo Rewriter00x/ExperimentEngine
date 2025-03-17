795d52b10264f65a7b0db7e79462553096a63c5e
Rewriter00x <colossus00x@icloud.com>
2025-03-17 09:03:26 +0200

    Added platform macros to PlatformDependencyInitializer initializing GLFW

--- ExperimentEngine/src/Platform/PlatformDependenciesInitializer.cpp
+++ ExperimentEngine/src/Platform/PlatformDependenciesInitializer.cpp
@@ -1,17 +1,23 @@
 #include "exppch.h"
 #include "PlatformDependenciesInitializer.h"
 
+#if defined(EXP_WINDOWS) || defined(EXP_MACOS)
 #include "GLFW/System/ExpGLFW.h"
+#endif
 
 namespace Exp::PlatformDependenciesInitializer
 {
 	void Init()
 	{
+#if defined(EXP_WINDOWS) || defined(EXP_MACOS)
 		ExpGLFW::Init();
+#endif
 	}
 
 	void Shutdown()
 	{
+#if defined(EXP_WINDOWS) || defined(EXP_MACOS)
 		ExpGLFW::Shutdown();
+#endif
 	}
 }
