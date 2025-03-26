07286293f0273024c03e3d0312bd14797df11a96
Rewriter00x <colossus00x@icloud.com>
2025-03-26 21:26:06 +0200

    Added input class

---
+++ ExperimentEngine/src/Engine/System/Input.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+namespace Exp
+{
+    namespace Input
+    {
+        bool IsKeyPressed(KeyCode key);
+
+        bool IsMouseButtonPressed(MouseCode button);
+        glm::vec2 GetMousePosition();
+        float GetMouseX();
+        float GetMouseY();
+
+    }
+}
---
+++ ExperimentEngine/src/Platform/GLFW/System/ExpGLFWInput.cpp
@@ -0,0 +1,40 @@
+﻿#include "exppch.h"
+#include "Engine/System/Input.h"
+
+#include "GLFW/glfw3.h"
+
+namespace Exp::Input
+{
+    bool IsKeyPressed(KeyCode key)
+    {
+        GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
+        const int state = glfwGetKey(window, (int)key);
+        return state == GLFW_PRESS || state == GLFW_REPEAT;
+    }
+
+    bool IsMouseButtonPressed(MouseCode button)
+    {
+        GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
+        const int state = glfwGetMouseButton(window, (int)button);
+        return state == GLFW_PRESS || state == GLFW_REPEAT;
+    }
+
+    glm::vec2 GetMousePosition()
+    {
+        GLFWwindow* window = static_cast<GLFWwindow*>(Application::Get().GetWindow()->GetNativeWindow());
+        double xPos, yPos;
+        glfwGetCursorPos(window, &xPos, &yPos);
+
+        return { (float)xPos, (float)yPos };
+    }
+
+    float GetMouseX()
+    {
+        return GetMousePosition().x;
+    }
+
+    float GetMouseY()
+    {
+        return GetMousePosition().y;
+    }
+}
