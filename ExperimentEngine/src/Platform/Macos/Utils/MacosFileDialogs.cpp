7dab6e7a7d0a42d69aafb19780d295d31a2dd3b1
Rewriter00x <colossus00x@icloud.com>
2025-04-16 22:03:15 +0300

    Added macos file dialogs

--- ExperimentEngine/src/Platform/Macos/Utils/MacosFileDialogs.cpp
+++ ExperimentEngine/src/Platform/Macos/Utils/MacosFileDialogs.cpp
@@ -1,14 +1,72 @@
-﻿#include "exppch.h"
+#include "exppch.h"
 #include "Engine/Utils/FileDialogs.h"
 
 namespace Exp::FileDialogs
 {
     std::filesystem::path OpenFile(const DialogData& data)
     {
-        return {};
+        std::string script = R"(
+        osascript -e '
+        try
+            set theFile to choose file default location ")" + data.DefaultPath.string() + R"("
+            POSIX path of theFile
+        on error
+            return ""
+        end try
+        '
+        )";
+
+        FILE* pipe = popen(script.c_str(), "r");
+        if (!pipe) return "";
+
+        char buffer[512];
+        std::string result;
+        while (fgets(buffer, sizeof(buffer), pipe)) {
+            result += buffer;
+        }
+        int status = pclose(pipe);
+        if (status)
+        {
+            return "";
+        }
+
+        if (!result.empty() && result.back() == '\n') {
+            result.pop_back();
+        }
+
+        return result;
     }
     std::filesystem::path SaveFile(const DialogData& data)
     {
-        return {};
+        const std::string script = R"(
+        osascript -e '
+        try
+            set theFile to choose file name default name ")" + data.DefaultName + R"(" default location ")" + data.DefaultPath.string() + R"("
+            POSIX path of theFile
+        on error
+            return ""
+        end try
+        '
+        )";
+
+        FILE* pipe = popen(script.c_str(), "r");
+        if (!pipe) return "";
+
+        char buffer[512];
+        std::string result;
+        while (fgets(buffer, sizeof(buffer), pipe)) {
+            result += buffer;
+        }
+        int status = pclose(pipe);
+        if (status)
+        {
+            return "";
+        }
+
+        if (!result.empty() && result.back() == '\n') {
+            result.pop_back();
+        }
+
+        return result;
     }
 }
