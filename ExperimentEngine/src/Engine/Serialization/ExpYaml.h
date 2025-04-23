cd7432684fdbbf4417e58d3b302b3667205c3333
Rewriter00x <colossus00x@icloud.com>
2025-04-23 22:09:47 +0300

    Removed unnecessary encode methods from yaml

--- ExperimentEngine/src/Engine/Serialization/ExpYaml.h
+++ ExperimentEngine/src/Engine/Serialization/ExpYaml.h
@@ -8,15 +8,6 @@ namespace YAML
     template<>
     struct convert<glm::vec2>
     {
-        static Node encode(const glm::vec2& rhs)
-        {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
         static bool decode(const Node& node, glm::vec2& rhs)
         {
             if (!node.IsSequence() || node.size() != 2)
@@ -31,16 +22,6 @@ namespace YAML
     template<>
     struct convert<glm::vec3>
     {
-        static Node encode(const glm::vec3& rhs)
-        {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.push_back(rhs.z);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
         static bool decode(const Node& node, glm::vec3& rhs)
         {
             if (!node.IsSequence() || node.size() != 3)
@@ -56,17 +37,6 @@ namespace YAML
     template<>
     struct convert<glm::vec4>
     {
-        static Node encode(const glm::vec4& rhs)
-        {
-            Node node;
-            node.push_back(rhs.x);
-            node.push_back(rhs.y);
-            node.push_back(rhs.z);
-            node.push_back(rhs.w);
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
         static bool decode(const Node& node, glm::vec4& rhs)
         {
             if (!node.IsSequence() || node.size() != 4)
@@ -83,14 +53,6 @@ namespace YAML
     template<>
     struct convert<Exp::Shared<Exp::Texture>>
     {
-        static Node encode(const Exp::Shared<Exp::Texture>& rhs)
-        {
-            Node node;
-            node.push_back(rhs->GetFilepath().string());
-            node.SetStyle(EmitterStyle::Flow);
-            return node;
-        }
-
         static bool decode(const Node& node, Exp::Shared<Exp::Texture>& rhs)
         {
             if (node.IsSequence())
